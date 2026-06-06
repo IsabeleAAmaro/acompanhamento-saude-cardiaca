0cda178f068b5b60b51cfd491cd6550efab1fb7c
Isabele Amaro <isabeleamaro@edu.unirio.br>
2026-06-06 08:48:56 -0300

    Create Registro Saude controller, response and test class for testing mocked user data

---
+++ src/main/java/com/saudecardiaca/backend/dtos/RelatorioSaudeResponse.java
@@ -0,0 +1,17 @@
+package com.saudecardiaca.backend.dtos;
+
+import com.saudecardiaca.backend.models.RegistroSaude;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RelatorioSaudeResponse {
+
+    private Long usuarioId;
+    private int totalRegistros;
+    private Double mediaFrequenciaCardiaca;
+    private Double mediaOxigenacao;
+
+    private List<RegistroSaude> historicoDetalhado;
+}
